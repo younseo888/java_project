573afe0641566dc655a0d9fb3371c518a253773a
younseo <part44480@gmail.com>
2025-06-06 11:40:45 +0900

    6/6(금)

--- java_project/src/java_project.java
+++ java_project/src/java_project.java
@@ -1,17 +1,26 @@
-import java.util.List;
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class java_project {
     public static void main(String[] args) {
-
-        List<String> storelist = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Store name");
-        String input = sc.nextLine();
+        while(true);
+            System.out.println("options: + e q enter");
+            String input = sc.nextLine();
 
-        storelist.add(input);
-        System.out.println(storelist);
+        if (input.equals("+")) {
+            System.out.println("name");
+            String name = sc.nextLine();
+            System.out.println("sit count");
+            int sit = sc.nextInt();
+            System.out.println("congestion");
+            String congestion = sc.nextLine();
+        }else if (input.equals("e")) {
+            System.out.println("name");
+            
+        }
+        )
     }
 }
